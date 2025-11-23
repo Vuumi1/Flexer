e8debbb687cd943261ad4a87f17b69dd0ceaa5d2
Vuumi1 <dshsh6121@gmail.com>
2025-11-23 21:34:22 +0200

    did some testing and added a few changes to the code editor

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,13 +1,25 @@
-let textArea = document.getElementById("codeWindow")
-const codeWindow = CodeMirror.fromTextArea(textArea, {
-    mode: "css",
-    lineNumbers: true,
-    tabSize: 2,
-    indentUnit: 2,
+import { EditorView, basicSetup } from "https://esm.sh/codemirror@6.0.2";
+import { css } from "https://esm.sh/@codemirror/lang-css@6.2.1";
+
+
+let styleSheet = document.getElementById("main-css").sheet;
+
+
+
+
+const cssEditor = new EditorView({
+    doc: "/*START*/",
+    extensions: [basicSetup, css()],
+    parent: document.getElementById("parentCodeWindow")
 });
 
 
 
-function getCode() {
-    console.log(codeWindow.getValue());
+function applyCode() {
+    const writtenCode = cssEditor.state.doc.toString();
+    styleSheet.insertRule(writtenCode, styleSheet.cssRules.length)
+    
 }
+
+
+
